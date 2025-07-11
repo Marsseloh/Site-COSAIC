3a652a8e3b2f9d29e1059c1e552fd9cf10ab33db
Marsseloh <marcelosantanacorreia2008@gmail.com>
2025-07-11 10:16:58 -0300

    VLibra

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "/Img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+        document.querySelector('.icon').src = "/Img/close_white_36dp.svg";
     }
 }
 
